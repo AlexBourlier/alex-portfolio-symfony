764b5b2f556eee6358fc3cb3a64945a76c78f551
Alexdessine <alexandre@bourlier.email>
2026-03-04 18:13:25 +0100

    Ajout de la structure de la page d'accueil

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,8 @@ import './stimulus_bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import './css/style.css';
 import './styles/app.css';
+import './js/main.js';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
