c1a8b806a06b7885788387271a91bf610bda0359
Alexdessine <alexandre@bourlier.email>
2026-03-05 10:02:42 +0100

    Modification du header et du footer dans le fichier base + restructuration HTML

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -6,14 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class AboutController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/about', name: 'app_about')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            'title' => 'Accueil - Mon portfolio'
+        return $this->render('about/index.html.twig', [
+            'controller_name' => 'AboutController',
+            'title' => 'À propos - Mon portfolio'
         ]);
     }
 }
