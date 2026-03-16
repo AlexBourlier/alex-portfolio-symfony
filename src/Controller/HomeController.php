35c7313e3785dfdc435e9690305d004872210b5e
Alexdessine <alexandre@bourlier.email>
2026-03-16 10:42:15 +0100

    Ajout des informations dynamique sur la page d'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,15 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\UserRepository;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
+        $users = $userRepository->findAllUsers();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'title' => 'Accueil - Mon portfolio'
+            'title' => 'Accueil - Mon portfolio',
+            'users' => $users,
         ]);
+
+
     }
 }
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -16,28 +16,21 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
-    //    /**
-    //     * @return User[] Returns an array of User objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('u.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?User
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    // Retourner les informations de la table user 
+    public function findAllUsers(): array
+    {
+        return $this->createQueryBuilder('u')
+            ->select(
+                'u.id, 
+                u.firstname, 
+                u.lastname,
+                u.phone,
+                u.poste,
+                u.city, 
+                u.aPropos, 
+                u.email'
+                )
+            ->getQuery()
+            ->getResult();
+    }
 }
