258588d4e9037dbcb5a1ada425484bf265c08714
Alexdessine <alexandre@bourlier.email>
2026-03-16 14:48:30 +0100

    Ajout des domaines pour chaques compétences

--- src/Controller/SkillsController.php
+++ src/Controller/SkillsController.php
@@ -5,15 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\DomainRepository;
 
 final class SkillsController extends AbstractController
 {
     #[Route('/skills', name: 'app_skills')]
-    public function index(): Response
+    public function index(DomainRepository $domainsRepository): Response
     {
+        $domains = $domainsRepository->findAllDomainsSkill();
+
         return $this->render('skills/index.html.twig', [
             'controller_name' => 'SkillsController',
-            'title' => 'Compétences - Mon portfolio'
+            'title' => 'Compétences - Mon portfolio',
+            'domains' => $domains,
         ]);
     }
 }
--- src/Repository/DomainRepository.php
+++ src/Repository/DomainRepository.php
@@ -16,28 +16,14 @@ class DomainRepository extends ServiceEntityRepository
         parent::__construct($registry, Domain::class);
     }
 
-    //    /**
-    //     * @return Domain[] Returns an array of Domain objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('d')
-    //            ->andWhere('d.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('d.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Domain
-    //    {
-    //        return $this->createQueryBuilder('d')
-    //            ->andWhere('d.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    // Retourner les catégories de compétences
+    public function findAllDomainsSkill(): array
+    {
+        return $this->createQueryBuilder('d')
+            ->select(
+                'd.title'
+            )
+            ->getQuery()
+            ->getResult();
+    }
 }
